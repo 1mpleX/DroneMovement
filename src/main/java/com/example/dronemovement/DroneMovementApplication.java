a338024ab5ed0c5f859c88e79837cea480bb8369
1mplex <csgoosnova2020@gmail.com>
2025-07-05 15:11:44 +0400

    init Drone project

---
+++ src/main/java/com/example/dronemovement/DroneMovementApplication.java
@@ -0,0 +1,13 @@
+package com.example.dronemovement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneMovementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneMovementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dronemovement/DroneMovementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dronemovement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneMovementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
